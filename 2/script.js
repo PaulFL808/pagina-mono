081bbfe9259c0200f0f51e01e7b14cbda5f4cfa1
PaulFL808 <paulferrada578@gmail.com>
2026-03-27 12:10:28 -0300

    subidos desde la nasa
    65 y kizazo es todo lo que necesitas kratos

---
+++ 2/script.js
@@ -0,0 +1,3 @@
+document.getElementById('dynamicButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').textContent = '¡Genial tenemos nuestra primera pagina del mono!';
+});
